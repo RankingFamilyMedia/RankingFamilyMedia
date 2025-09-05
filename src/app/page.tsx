e76c87afd57f958680d5c6d0f820e8de7037508a
Sugar Ranking <sugarranking@gmail.com>
2025-09-05 00:35:30 +0000

    only landing page css missing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
-import './video-background.css';
 import { LandingVideo } from "@/components/landing-video";
 
 export default function Home() {
